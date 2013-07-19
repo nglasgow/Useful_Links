a9de7db40565b32e4e845aac5a18e1875e3e2c22
nglasgow <nicole.glasgow@ymail.com>
2013-07-19 13:03:45 -0700

    correcting first commit

---
+++ app/models/resource.rb
@@ -0,0 +1,3 @@
+class Resource < ActiveRecord::Base
+  attr_accessible :description, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UsefulLinks::Application.config.secret_token = 'd1048e16c9235f76366e8d9718a6517d8d7c971604a7119f670d88a00b83192518e279bca7561bf8abbc3269cf1665c926a64218933a4dd59a375d37d6649691'
